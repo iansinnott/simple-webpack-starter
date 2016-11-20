f1abfb904bd8bcc052de01d422afd520bb45e40f
Ian Sinnott <ian@iansinnott.com>
2016-11-20 10:18:27 -0800

    Do not babel node modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,24 @@
 /**
  * Full Webpack docs at https://webpack.js.org
  */
+const path = require('path');
 
 module.exports = {
   devtool: 'cheap-module-source-map',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: './dist',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.js$/,
+        exclude: [ path.resolve(__dirname, 'node_modules') ],
         use: 'babel-loader',
       },
     ],
   },
 };
+
+
